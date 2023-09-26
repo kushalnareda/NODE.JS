e04c844e8f9c09245cf2329905b6f0514d2fda23
kushalnareda <bb1211214@iitd.ac.in>
2023-09-26 22:33:59 +0530

    Node.js Changes
    Setting up Node, Mysql

---
+++ learn-node/Basicsql/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  // The connection is now established.
+  // You can perform any necessary database operations here.
+});
