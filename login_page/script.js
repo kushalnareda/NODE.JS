d312f86d0f2ad1d6d314413215d32b90b531fea3
kushalnareda <bb1211214@iitd.ac.in>
2023-12-07 01:52:33 +0530

    Front endbasic

---
+++ login_page/script.js
@@ -0,0 +1,6 @@
+// JavaScript for handling form submission (no action for now)
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    // You can add your login logic here if needed in the future
+  });
+  
